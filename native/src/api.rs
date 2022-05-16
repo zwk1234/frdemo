64f8499d881dc911053a589149a6988327a91c6d
Your Name <you@example.com>
2022-05-16 22:46:44 +0800

    feat:add wasmer

--- native/src/api.rs
+++ native/src/api.rs
@@ -16,9 +16,33 @@ pub enum Platform {
     Wasm,
 }
 
+use std::time::Duration;
+use wasmer::{imports, Instance, Module, Store, Value};
 // A function definition in Rust. Similar to Dart, the return type must always be named
 // and is never inferred.
 pub fn platform() -> Platform {
+    let module_wat = r#"
+    (module
+      (type $t0 (func (param i32) (result i32)))
+      (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+        get_local $p0
+        i32.const 1
+        i32.add))
+    "#;
+    let store = Store::default();
+    let module = Module::new(&store, &module_wat).unwrap();
+    // The module doesn't import anything, so we create an empty import object.
+    let import_object = imports! {};
+    let instance = Instance::new(&module, &import_object).unwrap();
+
+    let add_one = instance.exports.get_function("add_one").unwrap();
+    let result = add_one.call(&[Value::I32(42)]).unwrap();
+    assert_eq!(result[0], Value::I32(43));
+    use std::thread::spawn;
+    let x = spawn(|| loop {
+        std::thread::sleep(Duration::from_secs(1));
+        println!("hello,android");
+    });
     // This is a macro, a special expression that expands into code. In Rust, all macros
     // end with an exclamation mark and can be invoked with all kinds of brackets (parentheses,
     // brackets and curly braces). However, certain conventions exist, for example the
